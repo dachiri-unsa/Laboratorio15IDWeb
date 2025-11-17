40266172961d546a6bf90ef9a04838188293dbd9
Xdaniel <dachiri@unsa.edu.pe>
2025-11-17 16:51:43 -0500

    inicio del proyecto

---
+++ Ejercicio03.js
@@ -0,0 +1,4 @@
+const primerElemento = document.getElementsByClassName("primer_elemento")[0];
+const textoPrimerElemento = primerElemento.textContent;
+console.log("El texto del primer elemento es: ");
+console.log(textoPrimerElemento);
